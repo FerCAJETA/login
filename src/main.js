c24ab0486fe7984101834d29d8cd372ba2c2cb2a
XFerTz <2402999@utrivieramaya.edu.mx>
2024-11-19 19:34:12 -0500

    Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,27 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5H0a0HYeqmuWZVR9YsMhcB-SM_zyhFxI",
+  authDomain: "login-163dd.firebaseapp.com",
+  projectId: "login-163dd",
+  storageBucket: "login-163dd.firebasestorage.app",
+  messagingSenderId: "604869144996",
+  appId: "1:604869144996:web:31c2533e29c1e04fbe1131"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
